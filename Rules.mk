140c2d44139c49ae382692a9563a44c8412d1274
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2023-02-07 20:49:23 +0100

    Removed the shared library submakefile from the build process.

--- Rules.mk
+++ Rules.mk
@@ -1,3 +1,3 @@
 CFLAGS       := -std=c99 -O2 -Wall -Wextra -W -pedantic -march=native -fPIC
 INCDIRS      := src
-SUBMAKEFILES := src/Rules.mk src/Rules-shared.mk examples/Rules.mk tests/Rules.mk
+SUBMAKEFILES := src/Rules.mk examples/Rules.mk tests/Rules.mk
